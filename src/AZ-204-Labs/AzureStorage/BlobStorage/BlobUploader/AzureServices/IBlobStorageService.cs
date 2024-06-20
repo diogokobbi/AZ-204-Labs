8a2308e9d01a8906928735eb6f3b211ab8153ca2
Diogo Kobbi <diogokobbi@gmail.com>
2024-06-20 00:25:31 -0300

    BlobUploader: Razor pages project to list and upload blobs;

---
+++ src/AZ-204-Labs/AzureStorage/BlobStorage/BlobUploader/AzureServices/IBlobStorageService.cs
@@ -0,0 +1,9 @@
+﻿namespace BlobUploader.AzureServices
+{
+    public interface IBlobStorageService
+    {
+        Task<IReadOnlyList<string>> GetBlobsNamesAsync();
+        Task<bool> PostBlobAsync(Stream fileStream, string name, bool OverwriteIfExists = false);
+        Task<bool?> AnyBlobAsync(string name);
+    }
+}

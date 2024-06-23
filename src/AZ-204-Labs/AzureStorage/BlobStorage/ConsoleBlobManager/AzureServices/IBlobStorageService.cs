eef0894a34b609b77332cb264505963631c69398
Diogo Kobbi <diogokobbi@gmail.com>
2024-06-23 16:53:29 -0300

    ConsoleBlobStorage project

---
+++ src/AZ-204-Labs/AzureStorage/BlobStorage/ConsoleBlobManager/AzureServices/IBlobStorageService.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleBlobManager.AzureServices
+{
+    public interface IBlobStorageService
+    {
+        Task<string?> GetAccountNameAsync();
+        Task<IReadOnlyList<string>> GetContainersNamesAsync();
+        Task<IReadOnlyList<string>> GetBlobsNamesAsync();
+        Task<bool> PostBlobAsync(Stream fileStream, string name, bool OverwriteIfExists = false);
+        Task<bool?> AnyBlobAsync(string name);
+    }
+}
